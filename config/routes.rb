c3120ee9642e94329d2685a000051cb0fa27fd11
Raymond Klauwer <klauwerraymond@gmail.com>
2022-01-06 09:26:01 -0600

    updated a lot of stuf

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
+  get "/gratis", to: "pages#gratis"
+  get "/over-mij", to: "pages#over-mij"
+  get "/werken-met-mij", to: "pages#werken-met-mij"
+  get "/contact", to: "pages#contact"
+  get "/bedankt-ebook", to: "pages#bedankt-ebook"
+  get "/online-programma", to: "pages#online-programma"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
